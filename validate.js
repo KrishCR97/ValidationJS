928a6e2b11afbd68a414a1c7a80e1cf7d70636cd
Veera Kishon Kumar Mucherla <veerakishon.mucherla@gmail.com>
2018-05-31 20:21:42 -0400

    Added css file and added validations to the css file.

--- validate.js
+++ validate.js
@@ -1,15 +1,28 @@
-var fnFlag = false;
+function createWarning(idValue,newIdValue){
+    if(document.getElementById(idValue).value == "" && document.getElementById(newIdValue ) == null){
+        document.getElementById(idValue).insertAdjacentHTML("afterend","<p id = "+newIdValue +"><br/> Enter a valid "+idValue+"</p>");
+    }
+    else if(document.getElementById(idValue).value != ""){
+        if(document.getElementById(newIdValue )){
+            document.getElementById(newIdValue ).remove();
+        }
+    return true;
+}
+return false;
+}
 document.getElementById("button").addEventListener('click',()=>{
-var data = "";
-var fnRefernce = document.getElementById("fn");
-
-data = fnRefernce.value;
-if(data == "" && !fnFlag){
-    fnFlag = true
-    fnRefernce.insertAdjacentHTML("afterend","<p id = 'p1'><br/> Enter a valid firstName</p>");
+    if(document.getElementById("rbtn1").checked || document.getElementById("rbtn2").checked){
+        if(document.getElementById(newIdValue )){
+            document.getElementById(newIdValue ).remove();
+        }   
 }
-else if(data!= "" && fnFlag){
-    fnRefernce
-}
+    else{
+        if(document.getElementById("rwarn") == null){
+            document.getElementById("button").insertAdjacentHTML("beforeBegin","<p id = 'rwarn'><br/> Enter a valid choice</p>");
+        }
 
+}
+    if(createWarning("firstName","w1")&createWarning("lastName","w2")&
+    createWarning("emailID","w3")&createWarning("phoneNumber","w4")){
+        }
 });
